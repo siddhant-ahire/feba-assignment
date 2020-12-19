e401275aae40e76d96887cf553fb62f4e313d456
Siddhant <siddhantahireofficial@gmail.com>
2020-12-19 18:24:27 +0530

    done the front-end work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
+import { Col, Container, Grid, Row,  } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
+import Card from './components/Card';
+import './styles/main.scss'
+
+const card1 = {
+      image:'https://images.indianexpress.com/2020/07/bridal-makeup-dos-and-donts_759.jpg',
+      tag:'SUPER VENDOR',
+      city:'Mumbai',
+      name:'Jssmakeuovers',
+      price:15000,
+      type:'Bridal Makeup',
+      rating:5.2
+}
+const card2 = {
+  image:'https://static-bebeautiful-in.unileverservices.com/easy-bridal-makeup-look-for-an-indian-wedding-600x350-picmobhome.jpg',
+  tag:'SUPER VENDOR',
+  city:'Jaipur',
+  name:'jasmin',
+  price:15000,
+  type:'Bridal Makeup',
+  rating:5.7
+}
+const card3 = {
+  image:'https://www.avcj.com/IMG/450/22450/india-cosmetics-makeup-580x358.jpeg?1554262311',
+  tag:'SUPER VENDOR',
+  city:'Pune',
+  name:'radhika',
+  price:14000,
+  type:'Bridal Makeup',
+  rating:5.6
+}
+const card4 = {
+  image:'https://assets.vogue.in/photos/5ce42bc21dc2678296c78aae/master/pass/bridal-makeup-tips-images-wedding-makeup-tips.jpg',
+  tag:'SUPER VENDOR',
+  city:'aligad',
+  name:'siri',
+  price:15000,
+  type:'Bridal Makeup',
+  rating:5.6
+}
+const card5 = {
+  image:'https://im.idiva.com/content/2019/Mar/idiva_products-from-abh_lead1_980x457.jpg',
+  tag:'SUPER VENDOR',
+  city:'melbourne',
+  name:'tesajji',
+  price:1600,
+  type:'Bridal Makeup',
+  rating:6.6
+}
 
 function App() {
+
   return (
-    <div>
-      Start Project
-    </div>
+    <Container>
+      <Grid fluid>
+        <Row className="mt-3">
+          <Col md={6} sm={8}>
+            <Card {...card1}/>  
+          </Col>    
+          <Col md={6} sm={8}>
+            <Card {...card2}/>  
+          </Col>
+          <Col md={6} sm={8}>
+            <Card {...card3}/>  
+          </Col>
+          <Col md={6} sm={8}>
+            <Card {...card4}/>  
+          </Col>  
+          <Col md={6} sm={8}>
+            <Card {...card5}/>  
+          </Col> 
+        </Row>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,95 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useCallback, useRef, useState } from 'react'
+import { Alert, Button, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Icon, Modal, Panel, Schema } from 'rsuite';
+import Tag from 'rsuite/lib/Tag/Tag';
+import { useModalState } from '../custom-hooks';
+
+const {StringType} = Schema.Types;
+
+const model = Schema.Model({
+    name:StringType().isRequired('Chat name is required'),
+    description:StringType().isRequired('Description is required')
+})
+const INITIAL_VALUE = {
+    name:'',
+    description:''
+}
+
+const Card = ({image,city,price,rating,name,type}) => {
+
+    const {isOpen, open, close} = useModalState();
+    const [formValue, setFormValue] = useState(INITIAL_VALUE);
+    // const [isLoading, setIsLoading] = useState(false);
+    const formRef = useRef();
+    const onFormChange = useCallback((value) => {   
+      setFormValue(value);
+    },[])
+
+    const onSubmit = (ev) => {
+        if(ev.type ==='click'){
+            Alert.success('Data submited successfully', 4000);
+        }
+    }
+
+    return(
+    <div className="mt-3">
+      <div>
+        <Panel bodyFill style={{display: 'inline-block' , cursor:"pointer",position:'relative'}} onClick={open} >
+          <img src={image} className="image"/>
+          <Icon icon="gittip" size="3x" style={{color:'#FFFFFFE6',position:'absolute', right:20, top:10}}/>
+          <div className="mt-3">
+            <Tag style={{backgroundColor:'#BE2364',color:'#FFFFFF'}}>SUPER VENDOR</Tag>
+            <span style={{fontWeight: 'bold',display:'inline-block',width:75}} className="ml-2"> {city}</span>
+            <Icon icon="star" style={{color:'#BE2364'}} className="justify-content-end" /> {rating}
+          </div>
+          <div>
+            <p style={{color:"#444444", fontSize:25,fontWeight:'lighter'}}>{name}</p>
+          </div>
+          <div className="mt-1">
+            <span style={{fontWeight:'bold'}}>Rs.{price} </span>  / {type}
+          </div>
+        </Panel>
+      </div>
+    <Modal show={isOpen} onHide={close}>
+                <Modal.Header>
+                    <Modal.Title>New Chat Room</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Form 
+                        fluid 
+                        onChange={onFormChange} 
+                        formValue={formValue}
+                        model={model}
+                        ref={formRef}
+                        >
+                        <FormGroup>
+                            <ControlLabel>Name</ControlLabel>
+                            <FormControl name="name" placeholder="Enter name"/>
+                            <HelpBlock>Required</HelpBlock>
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>Email</ControlLabel>
+                            <FormControl name="email" type="email" placeholder="Enter email"/>
+                            <HelpBlock>Required</HelpBlock>
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>DOB</ControlLabel>
+                            <FormControl name="datePicker" type="date"/>
+                            <HelpBlock>Required</HelpBlock>
+                        </FormGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button block appearance="primary" onClick={onSubmit}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+          </div>)
+            
+    
+    }
+
+export default Card
+
+
